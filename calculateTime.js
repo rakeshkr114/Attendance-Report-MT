4bc782686298a949b90724919a22c7c550eedd83
rakeshkr114 <M1039158@mindtree.com>
2018-08-01 12:07:03 +0530

    Added required hours

--- calculateTime.js
+++ calculateTime.js
@@ -1,14 +1,13 @@
-//Written By: Rakesh Yadav
-//Created on: April 25th, 2018
+//Written By: @Rakqesh
+//Created on: July 31st, 2018
 
 
 window.onload = function() {
 
-//hrs_value=document.getElementById("hrs_value").value;
 
 chrome.tabs.query({ "url": "https://webapps.mindtree.com/MAS/forms/AttendanceReport.aspx*" }, function (tabs) {
 
-	// if no utube url, return
+	// if no AttendanceReport url is opened, return
 	if(tabs.length==0)
 		return;
 
@@ -16,28 +15,29 @@ chrome.tabs.query({ "url": "https://webapps.mindtree.com/MAS/forms/AttendanceRep
 	
 	chrome.tabs.executeScript(tab.id, {
 		code:	`var div_elm = document.querySelectorAll('*[class$="105"]');
+				var holydays=0
 				var count=div_elm.length;
 				//alert("hi");
 				//alert(count);
 				Total_hrs=0;
 				Total_min=0;
 				for (var i = 0; i < count; i++){
-					//alert(i);
-					//alert(Number(div_elm[i].outerText));
+					if(div_elm[i].parentElement.style.backgroundColor == "whitesmoke"){
+						holydays++;
+					}
 					Total_hrs+=Number(div_elm[i].outerText.split(":")[0]);
 					//alert(Total_hrs);
 					Total_min+=Number(div_elm[i].outerText.split(":")[1]);
 				}
-				//alert(Total_hrs);
-				//alert(Total_min);
+				var Total_Required_Hrs=(count-holydays)*9;
 				Total_hrs+=Math.floor(Total_min/60);
 				Total_min=Total_min%60;
-				var Total_Time=""+Total_hrs+":"+Total_min;
-				Total_Time;
+				var Logged_Hrs_By_Required_Hrs=""+Total_hrs+":"+Total_min+" / "+Total_Required_Hrs;
+				Logged_Hrs_By_Required_Hrs;
 				`
-	},function (Total_Time) { // Execute your code
-			document.getElementById("time").innerHTML=Total_Time;  // end of linking Enter btn
-	}); // end of chrome executescript for next btn title and play/pause btn attribute
+	},function (Logged_Hrs_By_Required_Hrs) { // Execute your code
+			document.getElementById("time").innerHTML=Logged_Hrs_By_Required_Hrs;  // Set value in my.html
+	}); // end of chrome executescript
 });  // tabs query end
 
 }  //window load end
